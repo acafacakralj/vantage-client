adf0787e7aa9917db2e8f5f83d07b1bd1cb1b938
Strahinja Mitrović <strahinja@devotionit.org>
2019-03-16 18:04:25 +0100

    Formatting fix

--- src/test/java/org/devotionit/vantage/ut/core/request/param/SlowMATypeTest.java
+++ src/test/java/org/devotionit/vantage/ut/core/request/param/SlowMATypeTest.java
@@ -8,7 +8,6 @@ import org.devotionit.vantage.core.request.param.SlowMAType;
 import org.junit.Test;
 
 public class SlowMATypeTest {
-
 	@Test
 	public void Should_CreateSlowMAType_When_DataIsValid() {
 		Stream.of(0, 1, 2, 3, 4, 5, 6, 7, 8).forEach(integer -> {
@@ -40,5 +39,4 @@ public class SlowMATypeTest {
 		assertNotNull(slowMAType);
 		assertEquals(testData, Integer.parseInt(slowMAType.getValue()));
 	}
-
 }
