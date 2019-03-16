cb049e87c93abb0269ec677a06eeabe5ea4b2411
Strahinja Mitrović <strahinja@devotionit.org>
2019-03-16 18:00:40 +0100

    Formatting fix

--- src/test/java/org/devotionit/vantage/ut/core/request/param/FastMATypeTest.java
+++ src/test/java/org/devotionit/vantage/ut/core/request/param/FastMATypeTest.java
@@ -8,7 +8,6 @@ import org.devotionit.vantage.core.request.param.FastMAType;
 import org.junit.Test;
 
 public class FastMATypeTest {
-
 	@Test
 	public void Should_CreateFastMAType_When_DataIsValid() {
 		Stream.of(0, 1, 2, 3, 4, 5, 6, 7, 8).forEach(integer -> {
@@ -40,5 +39,4 @@ public class FastMATypeTest {
 		assertNotNull(fastMAType);
 		assertEquals(testData, Integer.parseInt(fastMAType.getValue()));
 	}
-
 }
