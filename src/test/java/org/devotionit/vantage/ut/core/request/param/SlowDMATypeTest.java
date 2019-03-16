72e6a88ec2ccf46d9f275a4a3550051ce8efa8a7
Strahinja Mitrović <strahinja@devotionit.org>
2019-03-16 18:03:16 +0100

    Formatting fix

--- src/test/java/org/devotionit/vantage/ut/core/request/param/SlowDMATypeTest.java
+++ src/test/java/org/devotionit/vantage/ut/core/request/param/SlowDMATypeTest.java
@@ -8,7 +8,6 @@ import org.devotionit.vantage.core.request.param.SlowDMAType;
 import org.junit.Test;
 
 public class SlowDMATypeTest {
-
 	@Test
 	public void Should_CreateSlowDMAType_When_DataIsValid() {
 		Stream.of(0, 1, 2, 3, 4, 5, 6, 7, 8).forEach(integer -> {
